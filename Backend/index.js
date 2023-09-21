859a17ad25a419997ce764c6bc361b98e07369ec
LukasJ00 <lukas@janfelt.se>
2023-09-21 17:15:03 +0200

    new

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express"); //Importera biblioteket
+const app = express(); //Initiera servern
+
+app.use( express.json() );
+
+app.listen(3000, () => { //Lyssna på port 3000
+  console.log("Listening on port 3000 ...");
+});
+
+
+//Vid en GET request till endpoint ‘/’
+app.get('/', (request, response)=>{
+    console.log(request.body);
+    response.send('TEST'); //Ett response med text skickas tillbaka
+})
+
+app.post('/names', (req, res)=>{
+    console.log(req.body);
+    const names = req.body.names;
+    res.send('New name posted'+ JSON.stringify(req.body));
+
+})
+
+app.put('/calc', (requ, resp)=>{
+    console.log(requ.body.number);
+    const num = requ.body.number;
+    resp.send(num + ' times 10 equals to ' + (num*10));
+
+})
