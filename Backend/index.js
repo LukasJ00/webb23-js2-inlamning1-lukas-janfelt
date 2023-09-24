7b0b7913e1cf2dc861f4dcb94866f9983e35112e
LukasJ00 <lukas@janfelt.se>
2023-09-24 16:17:55 +0200

    new

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,54 @@
-const express = require("express"); //Importera biblioteket
-const app = express(); //Initiera servern
+// const express = require("express"); //Importera biblioteket
+// const app = express(); //Initiera servern
 
-app.use( express.json() );
+// app.use( express.json() );
 
-app.listen(4000, () => { //Lyssna på port 3000
-  console.log("Listening on port 4000 ...");
+// //Vid en GET request till endpoint ‘/’
+// app.get('/', (request, response)=>{
+//     console.log(request.body);
+//     response.send('TEST'); //Ett response med text skickas tillbaka
+// })
+
+// app.post('/names', (req, res)=>{
+//     console.log(req.body);
+//     const names = req.body.names;
+//     res.send('New name posted'+ JSON.stringify(req.body));
+
+// })
+
+// app.put('/calc', (requ, resp)=>{
+//     console.log(requ.body.number);
+//     const num = requ.body.number;
+//     resp.send(num + ' times 10 equals to ' + (num*10));
+
+// })
+
+
+const express = require("express"); //Importerar express från mode_modules
+const fs = require("fs");
+
+const app = express();
+app.use(express.json()); //tala om att vi ska hantera data i json-format
+app.use(function (req, res, next) {
+  //Tillåt requests från alla origins
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
+app.get('/movie', (req, res)=>{
+    console.log('Get request received at /movies');
 
-//Vid en GET request till endpoint ‘/’
-app.get('/', (request, response)=>{
-    console.log(request.body);
-    response.send('TEST'); //Ett response med text skickas tillbaka
+    const rawMovies = fs.readFileSync("./data/movies.json");
+    const movieArray = JSON.parse(rawMovies);
+    console.log(movieArray);
+
+    res.send(movieArray);
 })
 
-app.post('/names', (req, res)=>{
-    console.log(req.body);
-    const names = req.body.names;
-    res.send('New name posted'+ JSON.stringify(req.body));
-
-})
-
-app.put('/calc', (requ, resp)=>{
-    console.log(requ.body.number);
-    const num = requ.body.number;
-    resp.send(num + ' times 10 equals to ' + (num*10));
-
-})
+app.listen(4000, () => { //Lyssna på port 3000
+    console.log("Listening on port 4000 ...");
+  });
--- Frontend/src/modules/main.js
+++ Frontend/src/modules/main.js
@@ -93,4 +93,26 @@ function determineWinner(playerChoice, computerChoice) {
       document.getElementById("choices").innerText = "";
     }
 
-    
+    async function getMovies(){
+      const url = 'http://localhost:4000/movies';
+  
+      const response = await fetch(url);
+      const movies = await response.json();
+  
+      // console.log(movies);
+      return movies;
+  }
+  
+  function displayMovies(movieArray){
+      // console.log(movieArray);
+  
+      for(const movies of movieArray){
+          const {name, year, director, genre} = movies;
+  
+          const h1 = document.createElement('h1');
+          h1.innerText = name;
+          document.body.append(h1);
+      }
+  }
+  
+  getMovies().then( displayMovies );
