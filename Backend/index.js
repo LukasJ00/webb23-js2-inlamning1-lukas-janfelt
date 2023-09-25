f48160f96dcae9392010f494e4c3aadb7d8fcd1e
LukasJ00 <lukas@janfelt.se>
2023-09-25 14:33:44 +0200

    new

--- Backend/index.js
+++ Backend/index.js
@@ -1,65 +1,48 @@
-// const express = require("express"); //Importera biblioteket
-// const app = express(); //Initiera servern
-
-// app.use( express.json() );
-
-// //Vid en GET request till endpoint ‘/’
-// app.get('/', (request, response)=>{
-//     console.log(request.body);
-//     response.send('TEST'); //Ett response med text skickas tillbaka
-// })
-
-// app.post('/names', (req, res)=>{
-//     console.log(req.body);
-//     const names = req.body.names;
-//     res.send('New name posted'+ JSON.stringify(req.body));
-
-// })
-
-// app.put('/calc', (requ, resp)=>{
-//     console.log(requ.body.number);
-//     const num = requ.body.number;
-//     resp.send(num + ' times 10 equals to ' + (num*10));
-
-// })
-
-
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors"); // Lägg till Cors-paketet
 
 const app = express();
 app.use(express.json());
 
-// ... CORS- och GET-highscore-routen (som tidigare) ...
+// Konfigurera CORS för att tillåta begäranden från din frontend-domän
+app.use(cors({
+  origin: '*' // Kan användas av alla domäneer
+}));
+
+app.get('/', (req, res) => {
+  console.log('Get request received at /');
+
+  const rawHighscore = fs.readFileSync("./data/highscore.json");
+  const highscoreArray = JSON.parse(rawHighscore);
+  console.log(highscoreArray);
+
+  res.json(highscoreArray);
+});
 
 app.post('/highscore', (req, res) => {
   console.log('POST request received at /highscore');
+  const { playerName, playerScore } = req.body;
 
-  try {
-    const { name, points } = req.body;
-    const rawHighscore = fs.readFileSync("./data/highscore.json");
-    const highscoreArray = JSON.parse(rawHighscore);
+  const highscoreArray = readHighscore();
 
-    // Lägg till spelarens namn och poäng i highscore-listan
-    highscoreArray.push({ name, points });
+  // Lägg till spelarens namn och poäng i highscore-listan
+  highscoreArray.push({ playerName, playerScore });
 
-    // Sortera highscore-listan efter poäng i fallande ordning
-    highscoreArray.sort((a, b) => b.points - a.points);
+  // Sortera highscore-listan efter poäng i fallande ordning
+  highscoreArray.sort((a, b) => b.playerScore - a.playerScore);
 
-    // Begränsa highscore-listan till de fem bästa
-    const top5Highscore = highscoreArray.slice(0, 5);
+  // Begränsa highscore-listan till de fem bästa
+  const top5Highscore = highscoreArray.slice(0, 5);
 
-    // Skriv den uppdaterade highscore-listan till filen
-    fs.writeFileSync("./data/highscore.json", JSON.stringify(top5Highscore, null, 2));
+  // Skriv den uppdaterade highscore-listan till filen
+  writeHighscore(top5Highscore);
 
-    // Skicka tillbaka den uppdaterade highscore-listan som svar
-    res.json(top5Highscore);
-  } catch (error) {
-    console.error('Error updating highscore:', error);
-    res.status(500).json({ error: 'Could not update highscore' });
-  }
+  // Skicka tillbaka den uppdaterade highscore-listan som svar
+  res.json(top5Highscore);
 });
 
-app.listen(4000, () => { //Lyssna på port 4000
-    console.log("Listening on port 4000 ...");
-  });
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port} ...`);
+});
--- Frontend/src/modules/main.js
+++ Frontend/src/modules/main.js
@@ -9,28 +9,28 @@ const rockButton = document.getElementById("rockBtn");
 const scissorsButton = document.getElementById("scissorsBtn");
 const paperButton = document.getElementById("paperBtn");
 const playerScoreDisplay = document.getElementById("playerScore");
-const computerWins= document.getElementById("computerWins");
+const computerWins = document.getElementById("computerWins");
 
 // namninput event
 playerNameInput.addEventListener("input", (event) => {
-    playerName = event.target.value;
-  });
+  playerName = event.target.value;
+});
 
-  // knappklick event
+// knappklick event
 rockButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    playGame("sten");
-  });
-  scissorsButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    playGame("sax");
-  });
-  paperButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    playGame("påse");
-  });
+  event.preventDefault();
+  playGame("sten");
+});
+scissorsButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  playGame("sax");
+});
+paperButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  playGame("påse");
+});
 
-   //regler
+// regler
 function determineWinner(playerChoice, computerChoice) {
   if (
     (playerChoice === "sten" && computerChoice === "sax") ||
@@ -46,72 +46,110 @@ function determineWinner(playerChoice, computerChoice) {
     return "computer";
   } else {
     return "tie";
-  }}
+  }
+}
 
-  function playGame(playerChoice) {
-    const choices = ["sten", "sax", "påse"];
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  
-    // visa spelarens och datorns val
-    document.getElementById("choices").innerText = `${playerName}: ${playerChoice} | Dator: ${computerChoice}`;
-  
-    // uppdatera poängen baserat på vinnare
-    const winner = determineWinner(playerChoice, computerChoice);
-    if (winner === "player") {
-      playerScore++;
-    } else if (winner === "computer") {
-      computerScore++;
-    }
-  
-    // visa poängen
-    playerScoreDisplay.innerText = `${playerName} poäng: ${playerScore}`;
-  
-    // kolla om datorn vann
-    if (computerScore >= 1) {
-      showWinner("Dator");
-      setTimeout(() => {
-        restartGame();
-      } 
-      , 3000);  // 3 sek till omstart
-      return
-    }
-  
+function playGame(playerChoice) {
+  const choices = ["sten", "sax", "påse"];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+  // visa spelarens och datorns val
+  document.getElementById("choices").innerText = `${playerName}: ${playerChoice} | Dator: ${computerChoice}`;
+
+  // uppdatera poängen baserat på vinnare
+  const winner = determineWinner(playerChoice, computerChoice);
+  if (winner === "player") {
+    playerScore++;
+  } else if (winner === "computer") {
+    computerScore++;
   }
 
-    //visa vinnaren
-    function showWinner(winnerName) {
-      computerWins.innerText = `${winnerName} vann 😢 försök igen`;
-    }
+  // visa poängen
+  playerScoreDisplay.innerText = `${playerName} poäng: ${playerScore}`;
 
-    
+  // kolla om datorn vann
+  if (computerScore >= 1) {
+    showWinner("Dator");
+    setTimeout(() => {
+      restartGame();
+    }, 3000); // 3 sek till omstart
+    return;
+  }
+}
 
-    function restartGame() {
-      playerScore = 0;
-      computerScore = 0;
-      computerWins.innerText = ""; // Clear the "Datorn vann" message
-      playerScoreDisplay.innerText = `${playerName} poäng: ${playerScore}`;
-      document.getElementById("choices").innerText = "";
-    }
+// visa vinnaren
+function showWinner(winnerName) {
+  computerWins.innerText = `${winnerName} vann 😢 försök igen`;
+  displayHighscore(); // Uppdatera highscore-listan när en spelomgång är klar
+}
 
-    async function sendHighscore(playerName, playerScore) {
-      const url = 'http://localhost:4000/highscore';
-    
-      try {
-        const response = await fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ name: playerName, points: playerScore }),
-        });
-        const updatedHighscore = await response.json();
-    
-        // Uppdatera highscore-listan på webbsidan
-        displayHighscore(updatedHighscore);
-      } catch (error) {
-        console.error('Error sending highscore:', error);
-      }
-    }
-    
-    // Anropa sendHighscore() med spelarens namn och poäng när spelet är klart
-    sendHighscore(playerName, playerScore);
+// Funktion för att återställa spelet
+function restartGame() {
+  playerScore = 0;
+  computerScore = 0;
+  computerWins.innerText = ""; // Rensa meddelandet "Datorn vann"
+  playerScoreDisplay.innerText = `${playerName} poäng: ${playerScore}`;
+  document.getElementById("choices").innerText = "";
+}
+
+// Funktion som anropas när spelet är över och resultatet ska sparas i highscore-listan
+async function gameIsOver() {
+  // Anropa postHighscore för att spara resultatet i highscore-listan
+  await postHighscore(playerName, playerScore);
+
+  // Återställ spelet
+  restartGame();
+}
+
+// Funktion för att spara resultatet i highscore-listan
+async function postHighscore(playerName, playerScore) {
+  const url = 'http://localhost:4000/highscore';
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ playerName, playerScore }),
+    });
+
+    const highscoreList = await response.json();
+    // Uppdatera din highscore-lista i din användargränssnitt om så behövs
+  } catch (error) {
+    console.error('Error posting highscore:', error);
+  }
+}
+
+// Funktion för att hämta highscore-listan från servern
+async function getHighscore() {
+  const url = "http://localhost:4000/";
+
+  try {
+    const response = await fetch(url);
+    const highscoreList = await response.json();
+    return highscoreList;
+  } catch (error) {
+    console.error("Error fetching highscore:", error);
+    return [];
+  }
+}
+
+// Funktion för att visa highscore-listan
+async function displayHighscore() {
+  const highscoreList = await getHighscore();
+  const highscoreListContainer = document.getElementById("highscore-list");
+
+  // Rensa befintliga highscore-listan
+  highscoreListContainer.innerHTML = "";
+
+  // Loopa igenom highscore-listan och skapa listelement för varje spelare
+  highscoreList.forEach((player, index) => {
+    const listItem = document.createElement("li");
+    listItem.innerText = `${index + 1}. ${player.playerName}: ${player.playerScore}`;
+    highscoreListContainer.appendChild(listItem);
+  });
+}
+
+// Anropa displayHighscore när sidan laddas och efter att highscore har uppdaterats
+displayHighscore();
